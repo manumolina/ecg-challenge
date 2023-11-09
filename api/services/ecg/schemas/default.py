4dce5ec1dcab204876c378718ab540135ecace2c
media <manu.molinamu@gmail.com>
2023-11-09 19:46:20 +0100

    Tables joined (ECG-ECGLead)

--- api/services/ecg/schemas/default.py
+++ api/services/ecg/schemas/default.py
@@ -8,7 +8,7 @@ class ECG(DBMixin, table=True):
 
 
 class ECGLead(DBMixin, table=True):
-    ecg_id: uuid.UUID
+    ecg_id: uuid.UUID = Field(default=None, foreign_key="ecg.id")
     name: str
     total_samples: int
     signal: str
